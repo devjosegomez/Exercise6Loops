ce240e530f64cb93c8cfb6cc565e4d4a1310cd5d
devjosegomez <devjosegomez@gmail.com>
2019-10-14 13:01:55 -0500

    Added series 1 to 100, 100 to 1, odd numers from 100 to 200

--- src/com/exercise6loops/app/ForLoop.java
+++ src/com/exercise6loops/app/ForLoop.java
@@ -3,7 +3,34 @@ package com.exercise6loops.app;
 public class ForLoop {
 
 	public static void main(String[] args) {
-
+		//constant declaration
+		
+		//Variable declaration
+		
+		//Objects construction
+		System.out.println("Display series from 1 to 100");
+		//Create a series from 1 to 100 step by 1
+		for(int i=1; i<=100; i++) {
+			System.out.println(i);
+		}
+		
+		System.out.println("Display series from 100 to 1");
+		
+		//Create a series from 100 to 1 step by 1
+		for(int i=100; i>=1; i--) {
+			System.out.println(i);
+		}
+		
+		System.out.println("Display odd numbers from 100 to 200");
+		
+		//Create a series from 100 to 1 step by 1
+		for(int i=100; i<200; i++) {
+			
+			if(i%2==0) {
+				continue;
+			}
+			System.out.println(i);
+		}
 	}
 
 }
